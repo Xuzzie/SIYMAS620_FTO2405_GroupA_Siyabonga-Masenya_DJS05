5fceca1ebdf49ab7e74806262bd52f032c42562c
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-09-20 11:51:53 +0200

    creating store function

--- store.js
+++ store.js
@@ -1,2 +1,11 @@
-// Importing  the reducer from reducers js
-import { counterReducer } from "./reducers";
+// Import the reducer from redicers.js 
+import { counterReducer } from './reducer';
+
+// Create the store factory function
+export const createStore = (reducer) => {
+    let state;
+    let listeners = [];
+
+    // This will return the current state
+    const getState = () => state;
+
