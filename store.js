1866be591b46c74e0acf1bd2d88b33817b301745
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-09-20 11:59:35 +0200

    Added the  Initialize   state

--- store.js
+++ store.js
@@ -1,26 +1,30 @@
-// Import the reducer from redicers.js 
-import { counterReducer } from './reducer';
+// Import the reducer from redicers.js
+import { counterReducer } from "./reducer";
 
 // Create the store factory function
 export const createStore = (reducer) => {
-    let state;
-    let listeners = [];
+  let state;
+  let listeners = [];
 
-    // This will return the current state
-    const getState = () => state;
+  // This will return the current state
+  const getState = () => state;
 
-        // Dispatch an action and update the state
-        const dispatch = (action) => {
-            state = reducer(state, action);
-            listeners.forEach(listener => listener()); // Notify all subscribers
-        };
-    
-        //  This subscribes to state changes  // chat gpt here 
-        const subscribe = (listener) => {
-            listeners.push(listener);
-            return () => {
-                listeners = listeners.filter(l => l !== listener); // Unsubscribe if necessary
-            };
-        };
-    
+  // Dispatch an action and update the state
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener()); // Notify all subscribers
+  };
 
+  //  This subscribes to state changes  // chat gpt here
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter((l) => l !== listener); // Unsubscribe if necessary
+    };
+  };
+
+  // Initialize the state
+  dispatch({}); // Call dispatch once to set the initial state
+
+  return { getState, dispatch, subscribe };
+};
