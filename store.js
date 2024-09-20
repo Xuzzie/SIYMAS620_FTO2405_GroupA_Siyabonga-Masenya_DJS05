6d31a7c2d836de65161e542d161e0c39fbd33740
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-09-20 11:56:58 +0200

    added dispact an action to update and subscribe states

--- store.js
+++ store.js
@@ -9,3 +9,18 @@ export const createStore = (reducer) => {
     // This will return the current state
     const getState = () => state;
 
+        // Dispatch an action and update the state
+        const dispatch = (action) => {
+            state = reducer(state, action);
+            listeners.forEach(listener => listener()); // Notify all subscribers
+        };
+    
+        //  This subscribes to state changes  // chat gpt here 
+        const subscribe = (listener) => {
+            listeners.push(listener);
+            return () => {
+                listeners = listeners.filter(l => l !== listener); // Unsubscribe if necessary
+            };
+        };
+    
+
